72524b9be3b2892aba013d900950143d8d66bce2
Tawanda Munongo <ejmunongo@gmail.com>
2023-02-22 17:03:47 +0200

    improved version to ignore JavaScript

--- translate.py
+++ translate.py
@@ -1,36 +1,46 @@
 import os
 
+import translators as ts
 from bs4 import BeautifulSoup
-from googletrans import Translator
 
 target_language = 'hi'
-source_language = 'en'
+source_language = 'fr'
 
 # function to translate the text
 
 
 def translate_text(target, text):
-    translator = Translator()
     soup = BeautifulSoup(text, 'html.parser')
+    # ['html', 'body', 'h1', 'h2', 'h3', 'p', 'title', 'strong', 'li', 'a', 'button']
     for tag in soup.find_all(string=True):
         if tag != "\n":
-            translation = translator.translate(
-                tag, src=source_language, dest=target).text
-            tag.replace_with(translation)
+            if (len(tag) > 0 and len(tag) < 5000 and isinstance(tag, str)):
+                if ";" in tag or "JSON" in tag or "function" in tag or "{" in tag:
+                    continue
+                else:
+                    translation = ts.translate_text(
+                        query_text=tag, if_ignore_empty_query=True, to_language=target, translator='google')
+                    if (len(translation) > 0):
+                        tag.replace_with(translation)
     return str(soup)
 
 
 # recursively read all files in the directory and
 # subdirectories with ".html" extension
-for root, dirs, files in os.walk('.'):
+for root, dirs, files in os.walk('../xtras'):
     for file in files:
         if file.endswith('.html'):
-            with open(os.path.join(root, file), 'r+') as f:
+            with open(os.path.join(root, file), 'r+', encoding='utf-8') as f:
                 data = f.read()
+                print(data)
                 # use googletrans to translate the contents of the file
                 translated = translate_text(target_language, data)
-                # write the translated data back to the file
-                # clear the original contents
-                f.truncate(0)
-                with open(file, 'w'):
+                if (len(translated) > 0):
+                    print("updating file")
+                    # clear the original contents
+                    f.truncate(0)
+                    # write the translated data back to the file
                     f.write(translated)
+                    f.close()
+
+print("Done!")
